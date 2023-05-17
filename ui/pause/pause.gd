8e8c38cc90f172e9eb9dcfde542a62552032acbe
Michael Tintiuc <contact@michaeltintiuc.com>
2023-05-18 00:58:30 +0300

    Fix pause toggle via ui_cancel

--- ui/pause/pause.gd
+++ ui/pause/pause.gd
@@ -4,3 +4,4 @@ extends UiContainer
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel") and get_tree().paused:
 		Events.toggle_pause_game.emit()
+		get_viewport().set_input_as_handled()
